3e50b12e4e43c2bb2b0b2553086c3360fa6fe161
Jim <the.jim.farrugia@gmail.com>
2021-08-31 15:10:39 +1000

    add nav and footer sections (bare)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
       <header className="page-header">
         <h1>Cryptocurrency Tracker</h1>
       </header>
+      <nav className="coin-nav">
+        nav section
+      </nav>
+      {/* Search bar */}
+      {/* Price chart */}
+      <footer className="page-footer">
+        footer section
+      </footer>
     </div>
   );
 }
