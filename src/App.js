329cb68fd0d920d6431b13bfddf69e0518fec9ef
Jim <the.jim.farrugia@gmail.com>
2021-09-01 01:56:51 +1000

    install axios & make GET request

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import { default as axios } from 'axios';
+
+const data = axios.get('https://api.coingecko.com/api/v3/coins/markets', {
+    params: {
+      vs_currency: 'usd',
+      per_page: 10
+    }
+  })
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
 function App() {
   return (
     <div className="App">
@@ -5,7 +20,10 @@ function App() {
         <h1>Cryptocurrency Tracker</h1>
       </header>
       <nav className="coin-nav">
-        nav section
+        {/* 
+            - List the 10 most popular coins as links.
+            - Style the list.
+        */}
       </nav>
       {/* Search bar */}
       {/* Price chart */}
