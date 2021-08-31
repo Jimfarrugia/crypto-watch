151bce78bb226089d93023d9bbad17a9fbf56e74
Jim <the.jim.farrugia@gmail.com>
2021-08-31 12:16:27 +1000

    initialise react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="page-header">
+        <h1>Cryptocurrency Tracker</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
