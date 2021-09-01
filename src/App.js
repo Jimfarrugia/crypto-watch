f813ce6f885d58e4c12e5a01dd6a7febf4e6ebf4
Jim <the.jim.farrugia@gmail.com>
2021-09-01 17:04:28 +1000

    add controlled text field for search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { default as axios } from 'axios';
 
+// TODO - fetchCoinList - get a list of coin names and symbols ("GET -> /coins/list")
+// TODO - store list of coin names and symbols in state (coinList)
+
 function App() {
   const [coinNavData, setCoinNavData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("mysearchterm");
 
   const fetchCoinNavData = n => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets', {
@@ -19,6 +23,11 @@ function App() {
     fetchCoinNavData(10);
   }, []);
 
+  const handleSearchTermChange = e => {
+    setSearchTerm(e.target.value);
+    // TODO - update autocomplete suggestions here
+  }
+
   return (
     <div className="App">
       <header className="page-header">
@@ -31,7 +40,12 @@ function App() {
             return (
               <li key={coin.symbol}>
                 <a href="#" title={coin.id}>
-                  <img src={coin.image} alt={`${coin.id} icon`} height="32" width="32" />
+                  <img
+                    src={coin.image}
+                    alt={`${coin.id} icon`}
+                    height="32"
+                    width="32"
+                  />
                 </a>
               </li>
             )
@@ -40,6 +54,13 @@ function App() {
         </ul>
       </nav>
       {/* Search bar */}
+      <p className="search-field">
+        <input type="text" value={searchTerm} onChange={handleSearchTermChange} />
+        <button>Search</button>
+        {/* 
+        // TODO - onClick -> get price chart or display "could not find"
+        */}
+      </p>
       {/* Price chart */}
       <footer className="page-footer">
         footer section
