9354ca10b712726f27fa574e6766cb35a78524cf
Jim <the.jim.farrugia@gmail.com>
2021-09-04 19:49:45 +1000

    add links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,32 @@
 const Footer = () => {
-  return <footer className="page-footer">footer section</footer>;
+  return (
+    <footer className="page-footer">
+      <ul>
+        <li>
+          <a
+            href="https://github.com/Jimfarrugia/cryptocurrency-tracker"
+            target="blank"
+          >
+            Source Code
+          </a>
+        </li>
+        <li>
+          <a href="https://www.jimfarrugia.com.au" target="blank">
+            Jim Farrugia
+          </a>
+        </li>
+        <li>
+          Data provided by{" "}
+          <a
+            href="https://www.coingecko.com/en/api/documentation"
+            target="blank"
+          >
+            CoinGecko API
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Footer;
