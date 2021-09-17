3ab7344dcca5c68f066006c1906b5e0dbe3ff6d2
Jim <the.jim.farrugia@gmail.com>
2021-09-17 20:41:29 +1000

    style the react-select search field

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -16,14 +16,67 @@ const SearchBar = ({
         })
       : searchSuggestions;
 
-  // create styles object for react-select field
-
-  // remember to preserve attributes from the old search-field
+  const selectStyles = {
+    control: (styles, { isFocused }) => ({
+      ...styles,
+      fontSize: "1rem",
+      background: "none",
+      boxShadow: isFocused ? "0 0 0.25em #a239ca" : "none",
+      borderColor: isFocused ? "#a239ca" : "#813772",
+      "&:hover": {
+        borderColor: "#a239ca",
+        cursor: "text",
+      },
+    }),
+    option: (styles, { isFocused }) => ({
+      ...styles,
+      fontSize: "1rem",
+      backgroundColor: isFocused ? "#a239ca" : "#0e0b16",
+      color: "#e7dfdd",
+      borderBottom: "1px solid #813772",
+      "&:hover": {
+        backgroundColor: "#a239ca",
+        color: "#e7dfdd",
+        cursor: "pointer",
+      },
+      "&:last-child": {
+        borderBottom: "none",
+      },
+    }),
+    input: (styles) => ({
+      ...styles,
+      color: "#a239ca",
+    }),
+    placeholder: (styles) => ({
+      ...styles,
+      color: "#813772",
+    }),
+    singleValue: (styles) => ({
+      ...styles,
+      color: "#813772",
+    }),
+    dropdownIndicator: (styles) => ({
+      ...styles,
+      color: "#813772",
+    }),
+    indicatorSeparator: (styles) => ({
+      ...styles,
+      backgroundColor: "#813772",
+    }),
+    menu: (styles) => ({
+      ...styles,
+      margin: 0,
+      backgroundColor: "#0e0b16",
+      border: "1px solid #813772",
+      borderTop: "none",
+    }),
+  };
 
   return (
     <div className="search">
       <Select
         options={options}
+        styles={selectStyles}
         onInputChange={(e) => handleSearchInputChange(e)}
         onChange={(e) => handleSearchChange(e)}
         noOptionsMessage={() => null}
