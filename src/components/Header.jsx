1d3d5b8ad3d0afc620a9fccf016426fdbdaed317
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-04 02:14:22 +1100

    add signin handler to signin button & do not render button when logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "../contexts/AuthContext";
 
 const Header = () => {
+  const { loginWithGoogle, currentUser } = useAuth();
+
+  const handleSignIn = async () => {
+    try {
+      await loginWithGoogle();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className="page-header-wrapper">
       <header className="page-header">
         <h1 data-testid="page-heading">Crypto Watch</h1>
-        <button type="button" title="Sign In" className="signin-button">
-          <FontAwesomeIcon icon={faUser} />
-        </button>
+        {!currentUser && (
+          <button
+            type="button"
+            title="Sign In"
+            className="signin-button"
+            onClick={handleSignIn}
+          >
+            <FontAwesomeIcon icon={faUser} />
+          </button>
+        )}
       </header>
     </div>
   );
