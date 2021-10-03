7febf1a4c2a0466f800428a90c95d9ecfda08507
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-04 01:30:24 +1100

    add signin button to page header (UI only)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
   return (
-    <header className="page-header">
-      <h1 data-testid="page-heading">Crypto Watch</h1>
-    </header>
+    <div className="page-header-wrapper">
+      <header className="page-header">
+        <h1 data-testid="page-heading">Crypto Watch</h1>
+        <button type="button" title="Sign In" className="signin-button">
+          <FontAwesomeIcon icon={faUser} />
+        </button>
+      </header>
+    </div>
   );
 };
 
