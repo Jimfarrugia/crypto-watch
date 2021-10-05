ba58b7983dbd755c066d7a3ee9626ca26315dcae
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-05 15:43:16 +1100

    combine signin/logout buttons in header

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,61 +1,38 @@
-import { useAuth } from "../contexts/AuthContext";
-
-const Footer = () => {
-  const { currentUser, logout } = useAuth();
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  return (
-    <footer className="page-footer">
-      {currentUser && (
-        <p>
-          You are logged in as <strong>{currentUser.displayName}</strong>.
-          &nbsp;
-          <button type="button" title="Logout" onClick={handleLogout}>
-            Logout
-          </button>
-        </p>
-      )}
-      <ul>
-        <li>
-          <a
-            href="https://github.com/Jimfarrugia/cryptocurrency-tracker"
-            target="blank"
-            title="Source Code"
-          >
-            Source Code
-          </a>{" "}
-          on Github
-        </li>
-        <li>
-          Authored by{" "}
-          <a
-            href="https://www.jimfarrugia.com.au"
-            target="blank"
-            title="Jim Farrugia"
-          >
-            Jim Farrugia
-          </a>
-        </li>
-        <li>
-          Data provided by{" "}
-          <a
-            href="https://www.coingecko.com/en/api/documentation"
-            target="blank"
-            title="CoinGecko API"
-          >
-            CoinGecko API
-          </a>
-        </li>
-      </ul>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="page-footer">
+    <ul>
+      <li>
+        <a
+          href="https://github.com/Jimfarrugia/cryptocurrency-tracker"
+          target="blank"
+          title="Source Code"
+        >
+          Source Code
+        </a>{" "}
+        on Github
+      </li>
+      <li>
+        Authored by{" "}
+        <a
+          href="https://www.jimfarrugia.com.au"
+          target="blank"
+          title="Jim Farrugia"
+        >
+          Jim Farrugia
+        </a>
+      </li>
+      <li>
+        Data provided by{" "}
+        <a
+          href="https://www.coingecko.com/en/api/documentation"
+          target="blank"
+          title="CoinGecko API"
+        >
+          CoinGecko API
+        </a>
+      </li>
+    </ul>
+  </footer>
+);
 
 export default Footer;
