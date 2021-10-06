07d377e3552803f1b3ccfe332a28a5d05c146653
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-07 01:13:10 +1100

    implement react router

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import App from "./App";
+import PageNotFound from "./components/PageNotFound";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </AuthProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
