7e94fa4fe06ad2fd9fb69b04ebbf2b98fa8c9c60
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-17 11:31:42 +1100

    Save images that are fetched for Details view

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as faStarSolid } from "@fortawesome/free-solid-svg-icons";
 import { faStar as faStarOutline } from "@fortawesome/free-regular-svg-icons";
-import { currencySymbol, formatPriceNumber } from "../helpers";
+import {
+  currencySymbol,
+  formatPriceNumber,
+  saveImageToLocalStorage,
+} from "../helpers";
 import { useAuth } from "../contexts/AuthContext";
 
 const Details = ({
@@ -29,6 +34,12 @@ const Details = ({
     };
     return isFavorite ? handleRemoveFavorite(data) : handleNewFavorite(data);
   };
+
+  useEffect(() => {
+    if (coinData && !localStorage.getItem(coinData.id)) {
+      saveImageToLocalStorage(coinData.id, coinData.image);
+    }
+  }, [coinData]);
 
   return (
     <section className="details">
