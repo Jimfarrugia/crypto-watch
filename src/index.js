1768de01be26a87c0f1a25aff77a1d53fef8fe21
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-19 08:42:43 +1100

    Lazy load routes with React.lazy and Suspense

--- src/index.js
+++ src/index.js
@@ -3,28 +3,31 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
-import App from "./App";
 import Layout from "./components/Layout";
-import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
-import Account from "./components/Account";
-import ResetPassword from "./components/ResetPassword";
-import PageNotFound from "./components/PageNotFound";
 import "./index.css";
+
+const App = React.lazy(() => import("./App"));
+const SignIn = React.lazy(() => import("./components/SignIn"));
+const SignUp = React.lazy(() => import("./components/SignUp"));
+const Account = React.lazy(() => import("./components/Account"));
+const ResetPassword = React.lazy(() => import("./components/ResetPassword"));
+const PageNotFound = React.lazy(() => import("./components/PageNotFound"));
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
         <Layout>
-          <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/sign-in" component={SignIn} />
-            <Route path="/sign-up" component={SignUp} />
-            <PrivateRoute path="/account" component={Account} />
-            <Route path="/reset-password" component={ResetPassword} />
-            <Route component={PageNotFound} />
-          </Switch>
+          <React.Suspense fallback={<div className="loader" />}>
+            <Switch>
+              <Route exact path="/" component={App} />
+              <Route path="/sign-in" component={SignIn} />
+              <Route path="/sign-up" component={SignUp} />
+              <PrivateRoute path="/account" component={Account} />
+              <Route path="/reset-password" component={ResetPassword} />
+              <Route component={PageNotFound} />
+            </Switch>
+          </React.Suspense>
         </Layout>
       </AuthProvider>
     </Router>
