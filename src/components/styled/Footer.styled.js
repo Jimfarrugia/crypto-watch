9133436e67e2c5c0be17407933886bb0986058e0
Jim Farrugia <the.jim.farrugia@gmail.com>
2021-10-21 15:24:40 +1100

    Tweak Footer styling

--- src/components/styled/Footer.styled.js
+++ src/components/styled/Footer.styled.js
@@ -1,20 +1,22 @@
 import styled from "styled-components";
 
 export const FooterStyled = styled.div`
-  padding: 1.5rem 0;
+  padding: 1em 0;
+  margin-top: 2em;
+  border-top: 1px solid ${({ theme }) => theme.color.main.dark};
+  text-align: right;
 
   ul {
-    padding: 0.75rem 0 0;
     margin: 0;
+    padding: 0.5rem 0 0;
     font-size: 0.8rem;
+    line-height: 1.35;
     color: ${({ theme }) => theme.color.text.secondary};
-    border-top: 1px solid ${({ theme }) => theme.color.main.dark};
 
     li {
       display: block;
       position: relative;
       list-style: none;
-      text-align: right;
       padding: 0.5rem 0;
 
       button {
